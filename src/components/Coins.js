57480fbd48cc7bdd09f86818d41a102f922f2c22
yusufobr <yousarsif80@gmail.com>
2023-05-30 16:59:22 +0100

    finish the Coins styles

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import arrowIcon from "./assets/arrow.svg";
 
 function Coins() {
   const { coins } = useSelector((state) => state.coins);
@@ -11,14 +12,27 @@ function Coins() {
       {coins.map((coin) => (
         <div
           onClick={() => navigate(`/coin/${coin.id}`)}
-          className="flex flex-col h-[100%] gap-4 bg-gray-100 pb-[%100] relative"
+          className="flex flex-col h-[100%] gap-4 bg-gray-100 pb-[%100] relative cursor-pointer rounded"
         >
-          <div className="absolute w-full h-full flex items-center justify-center overflow-hidden">
-            <span className="text-3xl uppercase font-extrabold text-center text-gray-300 ">{coin.id}</span>
+          <div className="p-3 h-48 flex flex-col z-10 justify-between items-end">
+            <img width={25} alt="arrow" src={arrowIcon} />
+            <div className="flex flex-col items-end">
+              <span className="text-2xl font-semibold">{coin.name}</span>
+              <span>
+                <span>
+                  {parseFloat(coin.priceUsd).toLocaleString(undefined, {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })}{" "}
+                  USD
+                </span>
+              </span>
+            </div>
           </div>
-          <div className="p-2 h-32">
-            <span>{coin.symbol}</span>
-            <span>{coin.name}</span>
+          <div className="absolute w-full h-full flex items-center justify-center overflow-hidden">
+            <span className="text-3xl uppercase font-extrabold text-center text-gray-300 ">
+              {coin.id.slice(0, 16)}
+            </span>
           </div>
         </div>
       ))}
